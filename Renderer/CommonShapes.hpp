4e724daf18fb5c0feef2f901b6c0b50f3604d58f
PiPower <darthlord.pl@gmail.com>
2025-11-01 19:20:41 +0100

    added basic geometry

---
+++ Renderer/CommonShapes.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <DirectXMath.h>
+#include <vector>
+
+struct CommonVertex
+{
+    DirectX::XMFLOAT3 pos;
+    DirectX::XMFLOAT3 normal;
+    DirectX::XMFLOAT2 tex;
+};
+
+struct Geometry
+{
+    const std::vector<CommonVertex>* const vertecies;
+    const std::vector<uint16_t>* const indicies;
+};
+
+extern const Geometry box;
