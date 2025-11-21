f6ce2521bf8a73b29f2d26664d82578a4b1533ca
PiPower <darthlord.pl@gmail.com>
2025-11-21 23:30:42 +0100

    first steps in gjk algo

--- Physics/Intersection.cpp
+++ Physics/Intersection.cpp
@@ -2,9 +2,13 @@
 
 using namespace DirectX;
 
-static void DistanceSubalgorithm()
+static void DistanceSubalgorithm(XMFLOAT3* simplex, uint8_t idxCount, XMFLOAT4* lambdas, XMFLOAT3* newDir)
 {
-	
+	switch (idxCount)
+	{
+	default:
+		break;
+	}
 }
 
 static void GetSupport(Body* bodyA, Body* bodyB, const XMFLOAT3* dir, XMFLOAT3* support, float bias)
@@ -19,23 +23,57 @@ static void GetSupport(Body* bodyA, Body* bodyB, const XMFLOAT3* dir, XMFLOAT3* 
 	XMVECTOR VecSupportB = XMLoadFloat3(&supportB);
 
 	XMStoreFloat3(support, XMVectorSubtract(VecSupportA, VecSupportB));
-
-
 }
+
+static bool HasPoint(const XMFLOAT3* simplex, const XMFLOAT3* newPoint, const uint8_t idxCount)
+{
+	constexpr float eps = 0.0001;
+	XMVECTOR vecNewPoint = XMLoadFloat3(newPoint);
+	for (uint8_t i = 0; i < idxCount; i++)
+	{
+		XMVECTOR diffVec = XMLoadFloat3(&simplex[i]) - vecNewPoint;
+		float lengthSq;
+		XMStoreFloat(&lengthSq, XMVector3LengthSq(diffVec));
+		if (lengthSq < eps * eps)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 static bool GjkIntersectionTest(Body* bodyA, Body* bodyB, Contact* contact, float bias)
 {
 	XMFLOAT3 simplex[4];
-	uint8_t i = 1;
+	uint8_t idxCount = 1;
 	XMFLOAT3 dir = { 1, 0, 0 };
+	XMFLOAT4 lambdas = {};
 	XMFLOAT3 support;
 
-	GetSupport(bodyA, bodyB, &dir, &support, bias);
-	simplex[0] = support;
-	dir = { simplex[0].x * -1.0f,  simplex[0].y * -1.0f,  simplex[0].z * -1.0f };
-	while (true)
+	GetSupport(bodyA, bodyB, &dir, &simplex[0], bias);
+	XMVECTOR vecDir = XMLoadFloat3(&simplex[0]);
+	XMStoreFloat3(&dir,vecDir * -1.0f);
+	bool hasOrigin = false;
+	float closestDist = sqrtf(simplex[0].x * simplex[0].x + simplex[0].y * simplex[0].y + simplex[0].z * simplex[0].z);
+	while (!hasOrigin)
 	{
 		GetSupport(bodyA, bodyB, &dir, &support, bias);
+		if (HasPoint(simplex, &support, idxCount))
+		{
+			break;
+		}
+		simplex[idxCount] = support;
+		idxCount++;
 
+		float dotProd;
+		XMStoreFloat(&dotProd, XMVector3Dot(vecDir, XMLoadFloat3(&simplex[idxCount])));
+		if (dotProd)
+		{
+			break;
+		}
+
+		DistanceSubalgorithm(simplex, idxCount, &lambdas, &dir);
+		
 	}
 
 	return false;
